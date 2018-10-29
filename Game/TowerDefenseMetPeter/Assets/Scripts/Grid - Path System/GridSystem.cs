1d19f23164afd1ecfd88a3fe47977d63ba126ca0
PeterSchreuder_Laptop <24441@ma-web.nl>
2018-10-29 13:25:31 +0100

    Grid systeem toegevoegd

---
+++ Game/TowerDefenseMetPeter/Assets/Scripts/Grid - Path System/GridSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+    public int[,] _tiles;
+
+    public float Gap = 5;
+
+    public float gridWidth;
+    public float gridHeight;
+
+    void Start()
+    {
+        SetUpGrid();
+        SetUpTiles(gridWidth,gridHeight,0);
+    }
+
+    void SetUpGrid()
+    {
+        //_tiles = new int[,];
+
+    }
+
+    void SetUpTiles(float _w, float _h, TileType _type)
+    {
+        for (int i = 0; i < gridHeight; i++)//Horizontal
+        {
+            for (int j = 0; j < gridWidth; j++)//Vertical
+            {
+
+                GameObject tileObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                float tileW = tileObject.GetComponent<Renderer>().bounds.size.x;
+                float tileD = tileObject.GetComponent<Renderer>().bounds.size.z;
+                
+                
+                tileObject.transform.position = new Vector3((i * tileW) + Gap, 0, (j * tileD) + Gap);
+
+            }
+        }
+    }
+}
