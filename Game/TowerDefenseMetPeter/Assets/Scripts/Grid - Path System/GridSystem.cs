b13ef14599932437a95dd41fefbb9af3cf8b804e
PeterSchreuder_Laptop <24441@ma-web.nl>
2018-11-06 09:56:06 +0100

    Grid system / Tiles / Tower

--- Game/TowerDefenseMetPeter/Assets/Scripts/Grid - Path System/GridSystem.cs
+++ Game/TowerDefenseMetPeter/Assets/Scripts/Grid - Path System/GridSystem.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//[ExecuteInEditMode]// Run this script in the editor
+
 public class GridSystem : MonoBehaviour
 {
+    
     //2D array: Grid
     public int[,] Grid;
     public GameObject GridTileHolder;
-
-    
 
     //Grid Width & Height
     public int gridWidth;
@@ -23,23 +25,46 @@ public class GridSystem : MonoBehaviour
     void SetUpGrid(int _w, int _h)//How many tiles on W & H
     {
         Grid = new int[_w, _h];
-        GridTileHolder = new GameObject("Tile Holder");
 
+        //Create a pool for the tiles
+        if (GameObject.Find("Tile Holder") == null)//if the tile holder not already exists make it (This because of the "[ExecuteInEditMode]")
+            GridTileHolder = new GameObject("Tile Holder");
+
+    }
+
+    void Update()
+    {
+        CheckTiles();
     }
 
 
     void SetUpTiles(int _w, int _h)//How many tiles on W & H
     {
+        int tileCount = 0;
+
         for (int i = 0; i < _w; i++)//Horizontal
         {
             for (int j = 0; j < _h; j++)//Vertical
             {
-                
                 //Maak een nieuwe tile op de juiste i & j positie
-                Tile newTile = new Tile(GridTileHolder, i, j);
-
+                Tile newTile = new Tile(GridTileHolder, tileCount, i, j);
+                tileCount++;
             }
         }
+    }
+
+    void CheckTiles()
+    {
+
+    }
+
+
+    //-Test- Creating an object from resources
+    void CreateFromResources()
+    {
+        GameObject obj = Resources.Load("Tower") as GameObject;
+
+        Instantiate(obj);
     }
 
     
--- Game/TowerDefenseMetPeter/Assets/Scripts/Grid - Path System/Tile.cs
+++ Game/TowerDefenseMetPeter/Assets/Scripts/Grid - Path System/Tile.cs
@@ -2,35 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//[ExecuteInEditMode]
 public class Tile : MonoBehaviour
 {
     //[ExecuteInEditMode]
-    public float Gap = 5;
+    public float Gap = 50;
 
-    public Tile(GameObject _parent, int _x, int _y)
+    //Tile dimentions
+    public float tileW;//X
+    public float tileD;//Y
+
+    //2D coordinates in the grid
+    public int xPos;
+    public int yPos;
+
+    public int tileId;
+
+    public Tile(GameObject _parent, int _tileId, int _x, int _y)
     {
+        //Set the x / yPos to the arguments
+        xPos = _x;
+        yPos = _y;
+
+        tileId = _tileId;
         //Vector3 _position
         GameObject tileObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        float tileW = tileObject.GetComponent<Renderer>().bounds.size.x;
-        float tileD = tileObject.GetComponent<Renderer>().bounds.size.z;
+        tileW = tileObject.GetComponent<Renderer>().bounds.size.x;
+        tileD = tileObject.GetComponent<Renderer>().bounds.size.z;
+
+        //Add visual text to the object
+        AddTextToObject(_x + " , " + _y);
 
         //Ik gebruik de _y als Z-Positie omdat de Grid een 2D array is
         tileObject.transform.position = new Vector3((_x * tileW) + Gap, 0, (_y * tileD) + Gap);
 
-        ApplyRandomColor(tileObject);
+        ApplyRandomType(tileObject);
 
         tileObject.transform.parent = _parent.transform;
     }
 
-
-
-    private TileType ApplyRandomColor(GameObject _tile)
+    private void Update()
     {
-        TileType randomColor = (TileType)Random.Range(0, System.Enum.GetNames(typeof(TileType)).Length);
+        CheckForObjects();
+    }
+
+    //Check if an object is being placed on the tile
+    void CheckForObjects()
+    {
+        Debug.Log("Checking...");
+        var checkObj = GameObject.FindGameObjectWithTag("Tower");
+
+        if (checkObj != null)
+        {
+            Debug.Log("Found one");
+            if (Vector3.Distance(transform.position,checkObj.transform.position) < tileW)//If the object is within the tile
+            {
+                checkObj.transform.position = new Vector3(xPos + tileW / 2, 0, yPos + tileD / 2);
+                Debug.Log("Set object on pos");
+            }
+        }
+    }
+
+
+    private TileType ApplyRandomType(GameObject _tile)
+    {
+        TileType randomType = (TileType)Random.Range(0, System.Enum.GetNames(typeof(TileType)).Length);
 
         Renderer renderer = _tile.GetComponent<Renderer>();
 
-        switch (randomColor)
+        switch (randomType)
         {
             case TileType.Grass:
                 renderer.material.color = Color.green;
@@ -47,18 +87,21 @@ public class Tile : MonoBehaviour
             case TileType.Water:
                 renderer.material.color = Color.blue;
                 break;
-
-
         }
 
-        GiveTileName(_tile, randomColor);
+        GiveTileName(_tile, randomType);
 
-        return randomColor;
+        return randomType;
     }
 
     private void GiveTileName(GameObject _tileToName, TileType _tileType)
     {
         //_tileToName.name = _tileToName.name + " - " + _tileType.ToString();
         _tileToName.name = "Tile - " + _tileType.ToString();
+    }
+
+    void AddTextToObject(string _text)
+    {
+
     }
 }
