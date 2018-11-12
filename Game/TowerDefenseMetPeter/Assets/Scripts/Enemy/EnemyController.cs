59560203028ab45362bb2ed1e1cbca7968f662eb
Sjors Kaagman <sjorspnk@gmail.com>
2018-11-12 12:24:40 +0100

    Moved scripterrskfpikalmsdncjkgewia;

--- Game/TowerDefenseMetPeter/Assets/Scripts/Enemy/EnemyController.cs
+++ Game/TowerDefenseMetPeter/Assets/Scripts/Enemy/EnemyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathFinding : MonoBehaviour {
+public class EnemyController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
