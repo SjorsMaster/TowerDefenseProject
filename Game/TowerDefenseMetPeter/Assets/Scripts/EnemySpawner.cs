a635ce24c7605254be32703a25bad363bd1b46f9
Sjors Kaagman <sjorspnk@gmail.com>
2018-11-12 09:44:24 +0100

    Begin van 2 scriptjes, alleen enemy nog aanpassen en dan wat dingen linken, maar ik ben er niet zeker van of jij er in heb lopen werken.

---
+++ Game/TowerDefenseMetPeter/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	public void Go () {
+        for(int i = 0; i <10; i++)
+        {
+            StartCoroutine(Wait());
+            try
+            {
+                Instantiate(Resources.Load("Enemy"));
+            }
+            catch
+            {
+                Debug.LogWarning("Can't seem to find Enemy!");
+            }
+        }
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(.5f);
+    }
+}
